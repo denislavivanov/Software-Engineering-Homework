5ff9f9a41feb5b99810bce0ea3145b1487098b64
Denislav Ivanov <denislavivanov82@gmail.com>
2022-01-13 08:35:54 +0200

    Added stuff

---
+++ main.py
@@ -0,0 +1,34 @@
+import unittest
+import requests
+import json
+
+data = requests.post("https://tues2022.proxy.beeceptor.com/my/api/test").json()
+
+def min_temp():
+    temps = [record['temperature'] for record in data['data']]
+    return min(temps)
+
+def max_temp():
+    temps = [record['temperature'] for record in data['data']]
+    return max(temps)
+
+def avg_temp():
+    temps = [record['temperature'] for record in data['data']]
+    return sum(temps) / len(temps)
+
+class TestStringMethods(unittest.TestCase):
+    def test_min(self):
+        temps = [record['temperature'] for record in data['data']]
+        self.assertEqual(min_temp(), min(temps))
+
+    def test_avg(self):
+        temps = [record['temperature'] for record in data['data']]
+        self.assertEqual(avg_temp(), sum(temps) / len(temps))
+
+    def test_max(self):
+        temps = [record['temperature'] for record in data['data']]
+        self.assertEqual(max_temp(), max(temps))
+
+
+if __name__ == '__main__':
+    unittest.main()
